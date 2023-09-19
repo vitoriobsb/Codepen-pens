b1ee6d0f6a1bf20e11d9e0042671e8e9fe3d3b28
Vitório <145414615+CVFVprogramacoes@users.noreply.github.com>
2023-09-19 01:48:51 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Mudar a cor de fundo da página</title>
+  <!-- ADD CSS -->
+  <link rel="stylesheet" href="style.css">
+  <!-- ADD JS -->
+  <link rel="stylesheet" href="script.js">
+</head>
+  <body>
+    <main class="container">
+      <div>
+        <span>Selecione uma cor:</span>
+        <input type="color" onchange="onChangeBg()">
+      </div>
+    </main>
+    <script src="script.js"></script>
+  </body>
+</html>
